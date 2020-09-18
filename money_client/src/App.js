a22b7f61477a1e5978f5230f4eeffaee97facc07
teeman60 <taiye41@gmail.com>
2020-09-18 16:14:41 -0500

    first commit

---
+++ money_client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>
+        The Money App For Cool People
+      </h3>
+    </div>
+  );
+}
+
+export default App;
